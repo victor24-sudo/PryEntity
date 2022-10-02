b6b68d75f40b038f2d1af08e2a03084d3f8561a1
vic2488 <poncev625@gmail.com>
2022-10-02 12:12:43 -0500

    Agregar archivos de proyecto.

---
+++ PryEntity/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PryEntity.Models
+{
+    public partial class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Apellido { get; set; } = null!;
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
