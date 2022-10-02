3009977fc0e5da8ee670db23325957fab338cb97
vic2488 <poncev625@gmail.com>
2022-10-02 12:25:04 -0500

    Cambios en la cadena de conexion

--- PryEntity/Program.cs
+++ PryEntity/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PryEntity.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<WebContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("conexion")));
 
 var app = builder.Build();
 
