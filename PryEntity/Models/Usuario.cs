4193a5a0fcc0faa71272d6362ced87a229088f13
vic2488 <poncev625@gmail.com>
2022-10-02 13:10:57 -0500

    Cambios en el formato de la Fecha

--- PryEntity/Models/Usuario.cs
+++ PryEntity/Models/Usuario.cs
@@ -8,7 +8,7 @@ namespace PryEntity.Models
         public int Id { get; set; }
         public string Nombre { get; set; } = null!;
         public string Apellido { get; set; } = null!;
-        public DateTime FechaNacimiento { get; set; }
+        public DateOnly FechaNacimiento { get; set; }
         public string Telefono { get; set; } = null!;
         public string Email { get; set; } = null!;
     }
